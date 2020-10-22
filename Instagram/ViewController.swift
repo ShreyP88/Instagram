7f323e4b981367f7940bb4365c9898128acabefc
Shreyas Pant <shrey.pant88@gmail.com>
2020-10-22 15:31:58 -0400

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Shreyas Pant on 10/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
